388699adc1da5c50f4a9993da5f4ccec74edfb3c
Rohitheeyyy <rohitsonofarul@gmail.com>
2024-10-24 08:52:14 +0530

    Initial setup

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/Rohitheeyyy'
+      className='link footer__link'
+    >
+      Rohit Arulraj
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,68 @@
+﻿const header = {
+  homepage: 'https://Rohitheeyyy.github.io/',
+  title: 'Rohit',
+}
+
+const about = {
+  name: 'Rohit Arulraj',
+  role: 'Web Designer',
+  description: 'I am a UI/UX specialist with a strong foundation in Designing,Web Development and Video Editing. I am also skilled in effective team communication and coordination.',
+  // resume: '/Resume.pdf',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/rohit-arulraj-11bb85334/',
+    github: 'https://github.com/Rohitheeyyy',
+  },
+}
+
+const projects = [
+  {
+    name: 'Railway Reservation System using C',
+    description: 'A new system to simplify the railway reservation system , this helps the user to reserve a train ticket easier',
+    stack: ['C'],
+    sourceCode: 'https://github.com/Rohitheeyyy',
+    // livePreview: 'https://github.com',
+  },
+  {
+    name: 'Smart Social Media Handler',
+    description: 'A useful website for new startups and young entrepreneur to maintain multiple social media platform on a single page and manage them efficiently',
+    stack: ['React,Javascript'],
+    sourceCode: 'https://github.com/Rohitheeyyy',
+    // livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  'UIUX Designing',
+  'Web Designer',
+  'Web Development',
+  'Video Editing',
+  'Effective Team Communication',
+  'Skilled Team Coordination',
+]
+
+const education = [
+  {
+    period: '2023 - 2027',
+    institution: 'SRM UNIVERSITY, RAMAPURAM, CHENNAI',
+    degree: 'Undergraduate in Bachelor of Technology in Computer Science Engineering',
+    gpa: '8.24 GPA',
+  },
+  {
+    period: '2021 - 2023',
+    institution: 'Sri Krishnaswamy Matriculation Higher Secondary School, Chennai',
+    degree: 'HSC +1, +2',
+    notable: 'Taekwondo State Player : Awarded with GOLD medal on match organised by Madras District Taekwondo Association',
+  },
+]
+
+const certifications = [
+  'Oracle Java Foundations (OCT 2024) - Oracle',
+  'Java Basics (OCT 2024) - HackerRank',
+
+]
+
+const contact = {
+  email: 'rohitsonofarul@gmail.com',
+}
+
+export { header, about, projects, skills, education, certifications, contact }
